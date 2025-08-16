985d9aad92ac514e1e5eaf6dc7f50f2a0154fdd0
darknecrocities <parejasarronkian@gmail.com>
2025-08-16 19:11:43 +0800

    feat: setup web firebase credentials

--- foxhub/lib/firebase_options.dart
+++ foxhub/lib/firebase_options.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform, kIsWeb;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (defaultTargetPlatform == TargetPlatform.android) {
+    if (kIsWeb) {
+      return web;
+    } else if (defaultTargetPlatform == TargetPlatform.android) {
       return android;
     } else {
       throw UnsupportedError(
-        'DefaultFirebaseOptions are only supported for Android in this project.',
+        'DefaultFirebaseOptions are only supported for Android & Web in this project.',
       );
     }
   }
@@ -15,6 +17,14 @@ class DefaultFirebaseOptions {
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyAgtzq7hhfFykWUyKsAk8LFbb0x6ukh0cY',
     appId: '1:54567040829:android:be2cea3517b439f6f83754',
+    messagingSenderId: '54567040829',
+    projectId: 'foxhub-3b7bb',
+    storageBucket: 'foxhub-3b7bb.appspot.com',
+  );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyAgtzq7hhfFykWUyKsAk8LFbb0x6ukh0cY',
+    appId: '1:54567040829:web:e01a91d47a664383f83754',
     messagingSenderId: '54567040829',
     projectId: 'foxhub-3b7bb',
     storageBucket: 'foxhub-3b7bb.appspot.com',
